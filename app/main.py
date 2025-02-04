9bdd51805fcca6d136ae9d45a223168dec42abd4
✿(˘̩◡˘̩♡) Sirat <sirat.b99@gmail.com>
2025-02-04 11:53:06 +0530

    added basic CORS

--- app/main.py
+++ app/main.py
@@ -1,7 +1,21 @@
 from fastapi import FastAPI
 import uvicorn
 
+from fastapi.middleware.cors import CORSMiddleware
+
 app = FastAPI()
+
+origins = [
+    "http://localhost:1234",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 async def root():
