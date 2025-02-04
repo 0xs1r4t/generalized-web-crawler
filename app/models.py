e3ff98f69f4f5adabca183e8e901472b784e694d
✿(˘̩◡˘̩♡) Sirat <sirat.b99@gmail.com>
2025-02-04 12:35:54 +0530

    added models

---
+++ app/models.py
@@ -0,0 +1,19 @@
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class Product(Base):
+    __tablename__ = "products"
+
+    id = Column(Integer, primary_key=True, index=True)
+    domain = Column(String, index=True)
+    product_id = Column(String, index=True)
+    product_name = Column(String)
+    product_category = Column(String, nullable=True)
+    product_brand  = Column(String, nullable=True)
+    product_price = Column(Integer, nullable=True)
+    product_image_url = Column(String, nullable=True)
+    product_url = Column(String, unique=True, index=True)
+
+
